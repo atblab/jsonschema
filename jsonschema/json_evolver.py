d4634a361d40e0a995d663dc1f0fcef0ead2e84a
unknown <Renjith.R@WS1A280.ushustech.com>
2016-01-08 18:28:23 +0530

    Adding json evolver

---
+++ jsonschema/json_evolver.py
@@ -0,0 +1,63 @@
+from jsonschema import validators
+
+def evolve_json(instance, schema):
+    """ 
+    Evolves the JSON data before validating.
+    The provided new schema will compare with JSON data and evolves it.    
+    """
+    subschemas = [(instance, schema)]
+    while subschemas:
+        subinstance, subschema = subschemas.pop()
+        
+        DefaultSetter(subschema).validate(subinstance)
+        subschemas.extend(
+            (subinstance.setdefault(property_v, {}), subsubschema)
+            for property_v, subsubschema in
+            subschema.get("properties", {}).items()
+        )
+            
+
+def _evolve(validator, properties, instance, schema):
+    """ 
+    Checks aliases and new fields by iterating the items in properties and 
+    change the JSON data accordingly.
+    """
+    for property_val, subschema in properties.items():
+        if "alias" in subschema and property_val not in instance:
+            instance[property_val] = instance[subschema["alias"]]
+            del instance[subschema["alias"]]
+        if "default" in subschema:
+            instance.setdefault(property_val, subschema["default"])  
+    for temp_instance in instance.keys():
+            if temp_instance not in properties:
+                del instance[temp_instance]
+DefaultSetter = validators.create(
+    meta_schema={}, validators={"properties": _evolve},
+)
+
+def extend(validator_cls):
+    ''' 
+    Extended validator class to add new validations, 
+    Currently the JSON data evolution process is done before validation.
+    '''
+    Validator = validators.extend(
+        validator_cls, {
+#             "properties" : _properties_with_aliases(validator_cls),
+        }
+    )
+    
+    class Blueprinter(Validator):
+        def instantiate(self, data, blueprint):
+            self.validate(data)
+            return data
+
+    return Blueprinter
+
+
+def evolve(data, schema):
+    evolve_json(data, schema)
+    Validator = validators.validator_for(schema)    
+    blueprinter = extend(Validator)(schema)    
+    return blueprinter.instantiate(data, schema)
+
+
---
+++ jsonschema/test_evolver.py
@@ -0,0 +1,38 @@
+import json_evolver
+
+
+"""
+This is used to test the JsonEvolver implementation.
+
+"""
+
+data = {"A":"Value-A","B":{"B1":"ValueB1"}, "C":"ValueR", "D": ["valueD1", "ValueD2"]}
+schema = {
+    "type": "object",
+    "properties": {
+        "New-A": {
+            "alias": "A"
+        },
+        
+        "New-B": {
+            "alias":"B",
+            "type": "object",
+            "properties": {
+                "New-B1": {
+                    "alias": "B1"
+                },
+                "B2" : {
+                    "default": "ValueB2"
+                }
+            },
+            "required": ["New-B1"]      
+        },
+        "E" : {
+            "default": "ValueE"
+        }          
+    },
+    "required": ["New-A"]
+}
+
+json_evolver.evolve(data, schema)
+print "Evolved JSON -> ", data
